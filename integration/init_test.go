e20cbb6acd78127d922a1a9d7988abbfd652e3be
Daniel Thornton <dthornton@vmware.com>
2020-06-30 21:17:53 +0000

    add integration tests
    Signed-off-by: Josh Zarrabi <jzarrabi@vmware.com>

---
+++ integration/init_test.go
@@ -0,0 +1,104 @@
+package integration_test
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/paketo-buildpacks/occam"
+	"github.com/paketo-buildpacks/packit/pexec"
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
+)
+
+var phpBuildpack string
+
+func TestIntegration(t *testing.T) {
+	Expect := NewWithT(t).Expect
+
+	bash := pexec.NewExecutable("bash")
+	buffer := bytes.NewBuffer(nil)
+	err := bash.Execute(pexec.Execution{
+		Args:   []string{"-c", "../scripts/package.sh --version 1.2.3"},
+		Stdout: buffer,
+		Stderr: buffer,
+	})
+	Expect(err).NotTo(HaveOccurred(), buffer.String)
+
+	phpBuildpack, err = filepath.Abs("../build/buildpackage.cnb")
+	Expect(err).NotTo(HaveOccurred())
+
+	SetDefaultEventuallyTimeout(10 * time.Second)
+
+	suite := spec.New("Integration", spec.Parallel(), spec.Report(report.Terminal{}))
+	suite("composer with nginx", testPhpNginx)
+	suite("composer with httpd", testPhpHttpd)
+	suite.Run(t)
+}
+
+func ContainerLogs(id string) func() string {
+	docker := occam.NewDocker()
+
+	return func() string {
+		logs, _ := docker.Container.Logs.Execute(id)
+		return logs.String()
+	}
+}
+
+func BeAvailableAndReady() types.GomegaMatcher {
+	return &BeAvailableAndReadyMatcher{
+		Docker: occam.NewDocker(),
+	}
+}
+
+type BeAvailableAndReadyMatcher struct {
+	Docker occam.Docker
+}
+
+func (*BeAvailableAndReadyMatcher) Match(actual interface{}) (bool, error) {
+	container, ok := actual.(occam.Container)
+	if !ok {
+		return false, fmt.Errorf("BeAvailableMatcher expects an occam.Container, received %T", actual)
+	}
+
+	response, err := http.Get(fmt.Sprintf("http://localhost:%s", container.HostPort()))
+	if err != nil {
+		return false, nil
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return false, nil
+	}
+
+	defer response.Body.Close()
+
+	return true, nil
+}
+
+func (m *BeAvailableAndReadyMatcher) FailureMessage(actual interface{}) string {
+	container := actual.(occam.Container)
+	message := fmt.Sprintf("Expected\n\tdocker container id: %s\nto be available.", container.ID)
+
+	if logs, _ := m.Docker.Container.Logs.Execute(container.ID); logs != nil {
+		message = fmt.Sprintf("%s\n\nContainer logs:\n\n%s", message, logs)
+	}
+
+	return message
+}
+
+func (m *BeAvailableAndReadyMatcher) NegatedFailureMessage(actual interface{}) string {
+	container := actual.(occam.Container)
+	message := fmt.Sprintf("Expected\n\tdocker container id: %s\nnot to be available.", container.ID)
+
+	if logs, _ := m.Docker.Container.Logs.Execute(container.ID); logs != nil {
+		message = fmt.Sprintf("%s\n\nContainer logs:\n\n%s", message, logs)
+	}
+
+	return message
+}
---
+++ integration/php_httpd_test.go
@@ -0,0 +1,79 @@
+package integration_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"testing"
+
+	"github.com/paketo-buildpacks/occam"
+	"github.com/sclevine/spec"
+
+	. "github.com/onsi/gomega"
+	. "github.com/paketo-buildpacks/occam/matchers"
+)
+
+func testPhpHttpd(t *testing.T, context spec.G, it spec.S) {
+	var (
+		Expect     = NewWithT(t).Expect
+		Eventually = NewWithT(t).Eventually
+
+		pack   occam.Pack
+		docker occam.Docker
+	)
+
+	it.Before(func() {
+		pack = occam.NewPack()
+		docker = occam.NewDocker()
+	})
+
+	context("when building a php app using php-web, php-composer, and httpd", func() {
+		var (
+			image     occam.Image
+			container occam.Container
+
+			name string
+		)
+
+		it.Before(func() {
+			var err error
+			name, err = occam.RandomName()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		it.After(func() {
+			Expect(docker.Container.Remove.Execute(container.ID)).To(Succeed())
+			Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
+			Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
+		})
+
+		it("creates a working OCI image", func() {
+			var err error
+			var logs fmt.Stringer
+			image, logs, err = pack.WithNoColor().Build.
+				WithBuildpacks(phpBuildpack).
+				WithNoPull().
+				Execute(name, filepath.Join("testdata", "offline_composer_httpd"))
+			Expect(err).NotTo(HaveOccurred(), logs.String())
+
+			container, err = docker.Container.Run.Execute(image.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(container).Should(BeAvailableAndReady(), ContainerLogs(container.ID))
+
+			response, err := http.Get(fmt.Sprintf("http://localhost:%s", container.HostPort()))
+			Expect(err).NotTo(HaveOccurred())
+			defer response.Body.Close()
+
+			content, err := ioutil.ReadAll(response.Body)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(content)).To(MatchRegexp("This is an HTTPD app."))
+
+			Expect(logs).To(ContainLines(ContainSubstring("PHP Buildpack")))
+			Expect(logs).To(ContainLines(ContainSubstring("Apache HTTP Server Buildpack")))
+			Expect(logs).To(ContainLines(ContainSubstring("PHP Web Buildpack")))
+			Expect(logs).To(ContainLines(ContainSubstring("PHP Composer Buildpack")))
+		})
+	})
+}
