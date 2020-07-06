b241028cba679d3b69921e2f2eade6ef767849c4
dwillist <dthornton@pivotal.io>
2020-07-06 09:45:11 -0400

    un-comment image cleanup

--- integration/php_nginx_test.go
+++ integration/php_nginx_test.go
@@ -44,8 +44,7 @@ func testPhpNginx(t *testing.T, context spec.G, it spec.S) {
 
 		it.After(func() {
 			Expect(docker.Container.Remove.Execute(container.ID)).To(Succeed())
-			fmt.Printf("test image name %s\n", image.ID)
-			// Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
+			Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
 			Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
 		})
 
