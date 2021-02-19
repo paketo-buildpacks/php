f6eed28123f9ca1ed8642da88793eececaff93ec
Sophie Wigmore <swigmore@vmware.com>
2021-02-19 11:58:59 -0500

    Add Image Label Buildpack (#329)
    Signed-off-by: Timothy Hitchener <thitchener@vmware.com>

--- integration/php_httpd_test.go
+++ integration/php_httpd_test.go
@@ -85,6 +85,7 @@ func testPhpHttpd(t *testing.T, context spec.G, it spec.S) {
 			Expect(logs).To(ContainLines(ContainSubstring("PHP Composer Buildpack")))
 			Expect(logs).NotTo(ContainLines(ContainSubstring("Procfile Buildpack")))
 			Expect(logs).NotTo(ContainLines(ContainSubstring("Environment Variables Buildpack")))
+			Expect(logs).NotTo(ContainLines(ContainSubstring("Image Labels Buildpack")))
 		})
 
 		context("using optional utility buildpacks", func() {
@@ -92,7 +93,8 @@ func testPhpHttpd(t *testing.T, context spec.G, it spec.S) {
 				Expect(ioutil.WriteFile(filepath.Join(source, "Procfile"), []byte("web: procmgr /layers/paketo-buildpacks_php-web/php-web/procs.yml && sleep infinity"), 0644)).To(Succeed())
 			})
 
-			it("creates a working OCI image and uses the Procfile and Environment Variables buildpacks", func() {
+
+			it("creates a working OCI image and uses the Procfile, Environment Variables, and Image Labels buildpacks", func() {
 				var err error
 				var logs fmt.Stringer
 				image, logs, err = pack.WithNoColor().Build.
@@ -100,6 +102,7 @@ func testPhpHttpd(t *testing.T, context spec.G, it spec.S) {
 					WithPullPolicy("never").
 					WithEnv(map[string]string{
 						"BPE_SOME_VARIABLE": "stew-peas",
+						"BP_IMAGE_LABELS":   "cool-label=cool-value",
 					}).
 					Execute(name, source)
 				Expect(err).NotTo(HaveOccurred(), logs.String())
@@ -121,9 +124,11 @@ func testPhpHttpd(t *testing.T, context spec.G, it spec.S) {
 				Expect(logs).To(ContainLines(ContainSubstring("Procfile Buildpack")))
 				Expect(logs).To(ContainLines(ContainSubstring("web: procmgr /layers/paketo-buildpacks_php-web/php-web/procs.yml && sleep infinity")))
 				Expect(logs).To(ContainLines(ContainSubstring("Environment Variables Buildpack")))
+				Expect(logs).To(ContainLines(ContainSubstring("Image Labels Buildpack")))
 
 				Expect(image.Buildpacks[5].Key).To(Equal("paketo-buildpacks/environment-variables"))
 				Expect(image.Buildpacks[5].Layers["environment-variables"].Metadata["variables"]).To(Equal(map[string]interface{}{"SOME_VARIABLE": "stew-peas"}))
+				Expect(image.Labels["cool-label"]).To(Equal("cool-value"))
 			})
 
 		})
