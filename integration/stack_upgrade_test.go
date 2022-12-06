39657801c469f075b5e5061b3d040e4640b86b54
Sophie Wigmore <swigmore@vmware.com>
2022-12-06 17:47:37 -0500

    bump buildpack versions manually

--- integration/stack_upgrade_test.go
+++ integration/stack_upgrade_test.go
@@ -69,7 +69,7 @@ func testStackUpgrades(t *testing.T, context spec.G, it spec.S) {
 		)
 
 		it.Before(func() {
-			source, err = occam.Source(filepath.Join("testdata", "rails"))
+			source, err = occam.Source(filepath.Join("testdata", "vendored_composer_app"))
 			Expect(err).NotTo(HaveOccurred())
 		})
 
