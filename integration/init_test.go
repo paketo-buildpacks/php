d0806c39db29d68046d0b424c27af781f37232fe
Sophie Wigmore <swigmore@vmware.com>
2023-02-02 13:28:30 -0500

    tag session handler images separately per builder
    - this should reduce test flakiness due to pulling and removing the same
    image in parallel across builder tests

--- integration/init_test.go
+++ integration/init_test.go
@@ -1,6 +1,7 @@
 package integration_test
 
 import (
+	"fmt"
 	"os/exec"
 	"path/filepath"
 	"testing"
@@ -14,10 +15,16 @@ import (
 	"github.com/onsi/gomega/format"
 )
 
-var phpBuildpack string
+var (
+	phpBuildpack   string
+	builder        occam.Builder
+	memcachedImage string
+	redisImage     string
+)
 
 func TestIntegration(t *testing.T) {
 	pack := occam.NewPack()
+	docker := occam.NewDocker()
 	Expect := NewWithT(t).Expect
 
 	format.MaxLength = 0
@@ -30,8 +37,22 @@ func TestIntegration(t *testing.T) {
 
 	SetDefaultEventuallyTimeout(10 * time.Second)
 
-	builder, err := pack.Builder.Inspect.Execute()
+	builder, err = pack.Builder.Inspect.Execute()
 	Expect(err).NotTo(HaveOccurred())
+
+	// pull and re-tag memcached image with builder-specific naming
+	// this will prevent flakes in which we try to reference/remove the same image in parallel bionic/jammy builder tests
+	memcachedImage = fmt.Sprintf("memcached-%s:latest", builder.LocalInfo.Stack.ID)
+	Expect(docker.Pull.Execute("memcached:latest")).To(Succeed())
+	Expect(docker.Image.Tag.Execute("memcached:latest", memcachedImage)).To(Succeed())
+	Expect(docker.Image.Remove.WithForce().Execute("memcached:latest")).To(Succeed())
+
+	// pull and re-tag redis image with builder-specific naming
+	// this will prevent flakes in which we try to reference/remove the same image in parallel bionic/jammy builder tests
+	redisImage = fmt.Sprintf("redis-%s:latest", builder.LocalInfo.Stack.ID)
+	Expect(docker.Pull.Execute("redis:latest")).To(Succeed())
+	Expect(docker.Image.Tag.Execute("redis:latest", redisImage)).To(Succeed())
+	Expect(docker.Image.Remove.WithForce().Execute("redis:latest")).To(Succeed())
 
 	suite := spec.New("Integration", spec.Parallel(), spec.Report(report.Terminal{}))
 
@@ -49,4 +70,9 @@ func TestIntegration(t *testing.T) {
 	suite("Redis Session Handler", testRedisSessionHandler)
 	suite("Reproducible Builds", testReproducibleBuilds)
 	suite.Run(t)
+
+	// Clean up memcached image
+	Expect(docker.Image.Remove.WithForce().Execute(memcachedImage)).To(Succeed())
+	// Clean up redis image
+	Expect(docker.Image.Remove.WithForce().Execute(redisImage)).To(Succeed())
 }
